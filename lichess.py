5b2442926bf2fe73351b70eed9779b229243f6f2
Ian Fisher <ianfisher45@gmail.com>
2017-11-15 17:57:38 -0500

    Remove deprecated functionality from Profile class

--- lichess.py
+++ lichess.py
@@ -28,11 +28,7 @@ class Profile:
         self.blitz_games = 0
         self.classical_rating = 0
         self.classical_games = 0
-        self.all_games = []
-        self.openings = Counter()
-        self.first_moves_as_white = Counter()
-        self.responses_to_e4 = Counter()
-        self.responses_to_d4 = Counter()
+        self.games = []
         if build is True:
             self.build(**build_kwargs)
 
@@ -81,59 +77,27 @@ class Profile:
     def _process_game(self, game_json):
         if game_json['variant'] != 'standard' or not game_json['moves']:
             return
-        try:
-            self.openings[game_json['opening']['name']] += 1
-        except KeyError:
-            pass
-        moves_list = game_json['moves'].split(' ')
-        first_move = moves_list[0]
+        if game_json['status'] not in ('mate', 'resign', 'outoftime', 'stalemate', 'draw'):
+            return
+        # Split the `moves` string into a list for easier processing.
+        game_json['moves'] = game_json['moves'].split(' ')
+        # Add `user_color` and `user_result` information.
         if game_json['players']['white']['userId'] == self.username:
-            self.first_moves_as_white[first_move] += 1
-            if game_json['status'] in ('mate', 'resign', 'outoftime'):
-                self.all_games.append( (moves_list, 'white', game_json['winner']) )
-            elif game_json['status'] in ('stalemate', 'draw'):
-                self.all_games.append( (moves_list, 'white', None) )
+            game_json['user_color'] = True
         else:
-            if game_json['status'] in ('mate', 'resign', 'outoftime'):
-                self.all_games.append( (moves_list, 'black', game_json['winner']) )
-            elif game_json['status'] in ('stalemate', 'draw'):
-                self.all_games.append( (moves_list, 'black', None) )
-            if first_move == 'e4':
-                self.responses_to_e4[moves_list[1]] += 1
-            elif first_move == 'd4':
-                self.responses_to_d4[moves_list[1]] += 1
-
-    def filter_games(self, moves_so_far, you=True):
-        return [game for game in self.all_games if game[0][:len(moves_so_far)] == moves_so_far]
-
-    def prettyprint(self):
-        if self.real_name:
-            print('=== {0.username} ({0.real_name}) ===\n'.format(self))
+            game_json['user_color'] = False
+        if game_json['status'] in ('stalemate', 'draw'):
+            game_json['user_result'] = 'draw'
         else:
-            print('=== {0.username} ===\n'.format(self))
-        if self.bullet_games > 0:
-            print('Bullet rating: {0.bullet_rating} over {0.bullet_games} game(s)'.format(self))
-        if self.blitz_games > 0:
-            print('Blitz rating: {0.blitz_rating} over {0.blitz_games} game(s)'.format(self))
-        if self.classical_games > 0:
-            print('Classical rating: {0.classical_rating} over {0.classical_games} game(s)'.format(self))
-        if self.bullet_games > 0 or self.blitz_games > 0 or self.classical_games > 0:
-            print()
+            winner = True if game_json['winner'] == 'white' else False
+            if winner == game_json['user_color']:
+                game_json['user_result'] = 'win'
+            else:
+                game_json['user_result'] = 'loss'
+        self.games.append(game_json)
 
-        print('Favorite openings')
-        for i, (opening, count) in enumerate(self.openings.most_common(3), start=1):
-            print('  {}. {} ({} game{})'.format(i, opening, count, '' if count == 1 else 's'))
-        print()
-
-        print('Favorite responses to 1. e4')
-        for i, (move, count) in enumerate(self.responses_to_e4.most_common(3), start=1):
-            print('  {}. {} ({} game{})'.format(i, move, count, '' if count == 1 else 's'))
-        print()
-
-        print('Favorite responses to 1. d4')
-        for i, (move, count) in enumerate(self.responses_to_d4.most_common(3), start=1):
-            print('  {}. {} ({} game{})'.format(i, move, count, '' if count == 1 else 's'))
-        print()
+    def filter_games(self, moves_so_far):
+        return [game for game in self.games if game['moves'][:len(moves_so_far)] == moves_so_far]
 
 
 last_api_call = 0
@@ -201,24 +165,20 @@ class MoveTree:
         if len(self.children) > 0:
             # The next level has already been built.
             return
-        for moves, color, result in games:
-            move = moves[len(self.stack)]
+        for game in games:
+            move = game['moves'][len(self.stack)]
             try:
                 node = self.children[move]
             except KeyError:
                 node = MoveTree.from_parent(self, move)
                 self.children[move] = node
-            node.update_results(result)
-
-    def update_results(self, result):
-        self.total += 1
-        if result is None:
-            self.draws += 1
-        elif (result == 'white' and len(self.stack) % 2 == 1) or \
-             (result == 'black' and len(self.stack) % 2 == 0):
-            self.wins += 1
-        else:
-            self.losses += 1
+            node.total += 1
+            if game['user_result'] == 'draw':
+                node.draws += 1
+            elif game['user_result'] == 'win':
+                node.wins += 1
+            else:
+                node.losses += 1
 
 
 # I may switch this to use a more robust opening book, like the one in the python-chess package.
@@ -253,21 +213,20 @@ class MoveExplorer:
 
     def _init_everything_else(self, color):
         self.color = color
-        self.your_turn = (color == 'white')
+        self.your_turn = self.color
         self.opening = None
         # The ply at which the opening was determined. Used for backtracking.
         self.opening_ply = 0
         self.tree = MoveTree()
-        self.games = [g for g in self.profile.all_games if g[1] == self.color]
+        self.games = [g for g in self.profile.games if g['user_color'] == self.color]
         self.tree.build_next_level(self.games)
 
     def backtrack(self):
         if self.tree.parent is not None:
             self.tree = self.tree.parent
             self.your_turn = not self.your_turn
-            self.games = [g for g in self.profile.all_games
-                                  if g[0][:len(self.tree.stack)] == self.tree.stack and \
-                                     g[1] == self.color]
+            self.games = [g for g in self.profile.filter_games(self.tree.stack)
+                                  if g['user_color'] == self.color]
             if len(self.tree.stack) <= self.opening_ply:
                 self.opening = OPENING_NAMES.get(tuple(self.tree.stack))
                 if self.opening is None:
@@ -280,14 +239,15 @@ class MoveExplorer:
             raise ValueError from None
         else:
             self.your_turn = not self.your_turn
-            self.games = [g for g in self.games if g[0][:len(self.tree.stack)] == self.tree.stack]
+            self.games = [g for g in self.games
+                                  if g['moves'][:len(self.tree.stack)] == self.tree.stack]
             if self.opening is None:
                 self.opening = OPENING_NAMES.get(tuple(self.tree.stack))
                 self.opening_ply = len(self.tree.stack)
             self.tree.build_next_level(self.games)
 
     def flip(self):
-        self._init_everything_else('white' if color == 'black' else 'black')
+        self._init_everything_else(not self.color)
 
     def moves_so_far(self):
         return self.tree.stack
@@ -315,7 +275,7 @@ if __name__ == '__main__':
     else:
         username = input('Please enter your Lichess username: ').strip()
     print('\nLoading user data...\n')
-    explorer = MoveExplorer(username, 'white', verbose=args.verbose)
+    explorer = MoveExplorer(username, True, verbose=args.verbose)
     while True:
         if explorer.your_turn is True:
             print(format_pl('\nYOUR MOVES (from {} game{})', len(explorer.games)))
@@ -328,8 +288,6 @@ if __name__ == '__main__':
             # I believe that Ng3xe5+ (7 chars) is the longest possible chess move in strict
             # algebraic notation.
             print('{:7}'.format(move), end='')
-            if not explorer.your_turn:
-                wins, losses = losses, wins
             print(' (you won {:7,.2%}, drew {:7,.2%},'.format(wins, draws), end='')
             print(' and lost {:7,.2%}'.format(losses), end='')
             print(format_pl(', from {} game{})', node.total))
@@ -349,7 +307,7 @@ if __name__ == '__main__':
                 print('({})'.format(explorer.opening))
             else:
                 print()
-        response = input('{}>>> '.format(explorer.color)).strip()
+        response = input('{}>>> '.format('white' if explorer.color else 'black')).strip()
         if response.lower() == 'quit':
             break
         elif response.lower() == 'back':
